50b77c16996b0aa9246c7a17ba6121b0f8f3decf
Joey199-sudo <94124588+Joey199-sudo@users.noreply.github.com>
2023-12-16 14:39:03 +0200

    Added axios to js for displaing data

--- src/javascript.js
+++ src/javascript.js
@@ -3,7 +3,20 @@ function search(event) {
   event.preventDefault();
 
   let input = document.querySelector("#search");
-  console.log(input.value);
+  let city = input.value;
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showDetails);
+}
+
+function showDetails(response) {
+  let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = response.data.city;
+
+  let currentTemp = document.querySelector("#temp");
+  let currentTemp2 = Math.round(response.data.temperature.current);
+  currentTemp.innerHTML = currentTemp2;
 }
 
 let form = document.querySelector(".searchForm");
